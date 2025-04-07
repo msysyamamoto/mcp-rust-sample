040a41ae37a4153c4c9e6288f1448f478a3a1f6f
Masayasu Yamamoto <491754+msysyamamoto@users.noreply.github.com>
2025-04-07 20:19:48 +0900

    implement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,114 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use rmcp::{Error as McpError, RoleServer, ServerHandler, model::*, service::RequestContext, tool};
+use rmcp::{ServiceExt, transport::stdio};
+use serde_json::json;
+use tracing_subscriber::{self, EnvFilter};
+use uuid::Uuid;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Initialize the tracing subscriber with file and stdout logging
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env().add_directive(tracing::Level::DEBUG.into()))
+        .with_writer(std::io::stderr)
+        .with_ansi(false)
+        .init();
+
+    tracing::info!("Starting MCP server");
+
+    // Create an instance of my mcp service
+    let service = UUID::new().serve(stdio()).await.inspect_err(|e| {
+        tracing::error!("serving error: {:?}", e);
+    })?;
+
+    service.waiting().await?;
+    Ok(())
+}
+
+#[tool(tool_box)]
+impl UUID {
+    pub fn new() -> Self {
+        Self
+    }
+
+    #[tool(description = "Generate a UUID v4")]
+    fn gen_uuid(&self) -> Result<CallToolResult, McpError> {
+        let uuid = Uuid::new_v4();
+        Ok(CallToolResult::success(vec![Content::text(
+            uuid.to_string(),
+        )]))
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct UUID;
+
+#[tool(tool_box)]
+impl ServerHandler for UUID {
+    fn get_info(&self) -> ServerInfo {
+        ServerInfo {
+            protocol_version: ProtocolVersion::V_2024_11_05,
+            capabilities: ServerCapabilities::builder()
+                .enable_prompts()
+                .enable_resources()
+                .enable_tools()
+                .build(),
+            server_info: Implementation::from_build_env(),
+            instructions: Some("UUID v4を生成します.".into()),
+        }
+    }
+
+    async fn list_resources(
+        &self,
+        _request: PaginatedRequestParam,
+        _: RequestContext<RoleServer>,
+    ) -> Result<ListResourcesResult, McpError> {
+        Ok(ListResourcesResult {
+            resources: vec![],
+            next_cursor: None,
+        })
+    }
+
+    async fn read_resource(
+        &self,
+        ReadResourceRequestParam { uri }: ReadResourceRequestParam,
+        _: RequestContext<RoleServer>,
+    ) -> Result<ReadResourceResult, McpError> {
+        Err(McpError::resource_not_found(
+            "resource_not_found",
+            Some(json!({
+                "uri": uri
+            })),
+        ))
+    }
+
+    async fn list_prompts(
+        &self,
+        _request: PaginatedRequestParam,
+        _: RequestContext<RoleServer>,
+    ) -> Result<ListPromptsResult, McpError> {
+        Ok(ListPromptsResult {
+            next_cursor: None,
+            prompts: vec![],
+        })
+    }
+
+    async fn get_prompt(
+        &self,
+        GetPromptRequestParam { name, arguments: _ }: GetPromptRequestParam,
+        _: RequestContext<RoleServer>,
+    ) -> Result<GetPromptResult, McpError> {
+        Err(McpError::invalid_params("prompt not found", None))
+    }
+
+    async fn list_resource_templates(
+        &self,
+        _request: PaginatedRequestParam,
+        _: RequestContext<RoleServer>,
+    ) -> Result<ListResourceTemplatesResult, McpError> {
+        Ok(ListResourceTemplatesResult {
+            next_cursor: None,
+            resource_templates: Vec::new(),
+        })
+    }
 }
